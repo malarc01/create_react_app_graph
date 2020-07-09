b201b61bd634f9af269370ea6e00eab89de93e05
malarc01 <email@example.com>
2020-07-09 10:52:39 -0400

    add index.js for routes

--- react-app-graph/src/routes/index.js
+++ react-app-graph/src/routes/index.js
@@ -1,23 +1,54 @@
 import React from 'react';
-import { gql, graphql } from 'react-apollo';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+import decode from 'jwt-decode';
 
-const user = ({ id, username }) => (
-  <h1 key={id} >{username}</h1>
-);
+import Auth from './Auth';
+import Login from './Login';
+import Register from './Register';
 
-const Auth = ({ data: { allUsers = [] } }) => (
-  <div>
-    {allUsers.map(user)}
-  </div>
-);
-
-const query = gql`
-{
-  allUsers {
-    id
-    username
+const checkAuth = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  if (!token || !refreshToken) {
+    return false;
   }
-}
-`;
 
-export default graphql(query)(Auth);
+  try {
+    // { exp: 12903819203 }
+    const { exp } = decode(refreshToken);
+
+    if (exp < new Date().getTime() / 1000) {
+      return false;
+    }
+
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    checkAuth() ? (
+      <Component {...props} />
+    ) : (
+        <Redirect to={{ pathname: '/login' }} />
+      )
+  )} />
+)
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" render={props => <Login {...props} />} />
+      <Route exact path="/register" render={props => <Register {...props} />} />
+      <AuthRoute exact path="/auth" component={Auth} />
+    </Switch>
+  </BrowserRouter>
+);
